1bae482b4fd24e13873de3b8e8a1d416242464ed
Denis Iogansen <pofigizm@gmail.com>
2018-01-21 14:02:54 +0300

    configure jss

--- src/root.js
+++ src/root.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { AppContainer } from 'react-hot-loader'
+import { create } from 'jss'
+import preset from 'jss-preset-default'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { createGenerateClassName } from 'material-ui/styles'
+import Reboot from 'material-ui/Reboot'
 
 import App from './container'
 import getPage from './pages'
@@ -9,15 +14,22 @@ import { init } from './actions'
 import getStore from './store'
 import ownRoutes from './pages/routes'
 
+const generateClassName = createGenerateClassName()
+const jss = create(preset())
+
 const render = (Wrapper, Page, store, title, menu) => {
   const root = document.getElementById('app')
 
   ReactDOM.render(
     <AppContainer>
       <Provider store={store}>
-        <Wrapper title={title} menu={menu} >
-          <Page />
-        </Wrapper>
+        <JssProvider jss={jss} generateClassName={generateClassName}>
+          <Reboot>
+            <Wrapper title={title} menu={menu} >
+              <Page />
+            </Wrapper>
+          </Reboot>
+        </JssProvider>
       </Provider>
     </AppContainer>,
     root,
